fd43e3a95d226279f3cda226f13aabe9ead515b7
gitrahul9 <rahulsingal2009@gmail.com>
2015-12-01 11:47:29 +0530

    create finding square roots

---
+++ chef36_finding_sqaure_roots.c
@@ -0,0 +1,17 @@
+//finding sqaure roots
+#include<stdio.h>
+#include<math.h>
+int main(){
+int t;
+int n;
+
+scanf("%d",&t);
+while(t--){
+scanf("%d",&n);
+printf("%d\n",(int)sqrt(n));
+
+}
+return 0;
+
+}
+
